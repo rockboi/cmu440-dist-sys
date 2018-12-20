f99e2017586fe55c2e7d852df061a51ef3a9e3a1
Vinay Bhat <vinaybhat001@gmail.com>
2018-12-20 21:34:25 +0530

    Server working!

--- P0/src/github.com/cmu440/p0/server_impl.go
+++ P0/src/github.com/cmu440/p0/server_impl.go
@@ -21,7 +21,7 @@ type keyValueServer struct {
 
 type ReaderWriterCloser interface {
 	Read(p []byte) (n int, err error)
-	Write(p []byte) error
+	Write(p []byte) (n int, err error)
 	Close() error
 }
 
@@ -31,6 +31,7 @@ const (
 	GETRequest ClientRequestType = 0
 	PUTRequest ClientRequestType = 1
 	DELRequest ClientRequestType = 2
+	SlowClientsBuffer int = 500
 )
 
 type ClientRequest struct {
@@ -46,7 +47,7 @@ type ClientHandle struct {
 
 func (clientHandle *ClientHandle) Init() error {
 	clientHandle.reqChan = make(chan *ClientRequest)
-	clientHandle.resChan = make(chan string)
+	clientHandle.resChan = make(chan string, SlowClientsBuffer)
 	return nil
 }
 
@@ -71,6 +72,7 @@ func (kvs *keyValueServer) InitServer(port int) error {
 	kvs.writelock <- 1
 	kvs.activeconns = 0
 	kvs.droppedconns = 0
+	return nil
 }
 
 func (kvs *keyValueServer) StartListeningForConn() {
@@ -86,7 +88,7 @@ func (kvs *keyValueServer) StartListeningForConn() {
 		if err != nil {
 			fmt.Println("Couldn't accept: ", err)
 			os.Exit(-1)
-		}
+	}
 		go kvs.handleConn(conn)
 	}
 	fmt.Println("All done")
@@ -104,35 +106,41 @@ func (kvs *keyValueServer) handleConn(conn ReaderWriterCloser) {
 	// fmt.Println("Client sent: ", string(buf[0:n]))
 
 	for {
-		n, err := conn.Read(buf[0:n])
+		n, err := conn.Read(buf[:])
 		if err != nil { //EOF, or worse
 			break
 		}
-		clientHandle.reqChan <- ParseClientMessage(string(buf[0:n]))
-		conn.Write(<-clientHandle.resChan)
+		clientReq, err := ParseClientMessage(string(buf[0:n]))
+		if err != nil {
+			fmt.Println("Could not parse message: ", err)
+			conn.Write([]byte("Could not parse message\n"))
+			continue
+		}
+		clientHandle.reqChan <- clientReq
+		clientRes := <-clientHandle.resChan
+		conn.Write([]byte(clientRes))
 	}
+	kvs.activeconns--
 	kvs.droppedconns++
 }
 
 func ParseClientMessage(msg string) (clientReq *ClientRequest, err error) {
 	// Parse client string here
-	vals := strings.split(msg, ",")
+	var cleanmsg = strings.TrimSuffix(msg, "\n")
+	vals := strings.Split(cleanmsg, ",")
 	clientReq = new(ClientRequest)
-	if (vals[0] == "get") {
+	if (strings.Compare(vals[0], "get") == 0) {
 		clientReq.reqType = GETRequest
 		clientReq.key = vals[1]
-	}
-	else if (vals[0] == "delete") {
+	} else if (strings.Compare(vals[0], "delete") == 0) {
 		clientReq.reqType = DELRequest
 		clientReq.key = vals[1]
-	}
-	else if (vals[0] == "put") {
+	} else if (strings.Compare(vals[0], "put") == 0) {
 		clientReq.reqType = PUTRequest
 		clientReq.key = vals[1]
 		clientReq.value = vals[2]
-	}
-	else {
-		return 0, fmt.Errorf("Unknown request type %s", vals[0])
+	} else {
+		return nil, fmt.Errorf("Unknown request type %s", vals[0])
 	}
 	return clientReq, nil
 
@@ -142,17 +150,17 @@ func (kvs *keyValueServer) ProcessRequests() error {
 	for clientHandle := range kvs.globalreqchan {
 		go kvs.ClientHandler(clientHandle)
 	}
-
+	return nil
 }
 
 func (kvs *keyValueServer) ClientHandler(clientHandle *ClientHandle) error {
 	for req := range clientHandle.reqChan {
 		go kvs.RequestHandler(req, clientHandle.resChan)
 	}
+	return nil
 }
 
 func ConvertToSingleString(dbvalues []([]byte)) string {
-	var finalstr string
 	finalstr := ""
 	for _, val := range dbvalues {
 		finalstr += (string(val[:]) + "\n")
@@ -163,23 +171,24 @@ func ConvertToSingleString(dbvalues []([]byte)) string {
 func (kvs *keyValueServer) RequestHandler(clientRequest *ClientRequest, resChan chan string) error {
 	if (clientRequest.reqType == GETRequest) {
 		// get all values associated with key
-		resChan <- ConvertToSingleString(get(clientRequest.key))
-	}
-	else if (clientRequest.reqType == PUTRequest) {
+		respStr := ConvertToSingleString(get(clientRequest.key))
+		fmt.Printf("GET request, key: %s, value: %s.\n", clientRequest.key, respStr)
+		resChan <- respStr
+	} else if (clientRequest.reqType == PUTRequest) {
 		// take lock on server
 		<-kvs.writelock
-		put(clientRequest.key, clientRequest.value)
+		fmt.Printf("PUT request, key: %s, value: %s.\n", clientRequest.key, clientRequest.value)
+		put(clientRequest.key, []byte(clientRequest.value))
 		// give back lock
 		kvs.writelock <- 1
 		resChan <- ""
-	}
-	else if (clientRequest.reqType == DELRequest) {
+	} else if (clientRequest.reqType == DELRequest) {
 		//delete key
-		delete(clientRequest.key)
+		fmt.Printf("DELETE request, key: %s.\n", clientRequest.key)
+		clear(clientRequest.key)
 		resChan <- ""
-	}
-	else {
-		return 0, fmt.Errorf("Unsupported request type %g", clientRequest.reqType)
+	} else {
+		return fmt.Errorf("Unsupported request type %g", clientRequest.reqType)
 	}
 	return nil
 }
